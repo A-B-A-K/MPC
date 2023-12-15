e867b98c05a27f6bc164ce58a92157ca95ca1e5c
marpep <127655372+marpep@users.noreply.github.com>
2023-12-15 14:53:22 +0100

    +open loop formulation

--- rocket_project/Deliverable_3_1/Deliverable_3_1.m
+++ rocket_project/Deliverable_3_1/Deliverable_3_1.m
@@ -68,4 +68,32 @@ u_z = mpc_z.get_u(x0_z);
 [T, X_sub, U_sub] = rocket.simulate_f(sysz, x0_z, Tf, @mpc_z.get_u, 0);
 ph = rocket.plotvis_sub(T, X_sub, U_sub, sysz, xs, us);
 
+% Open-loop
 
+%sysx
+[u_x, T_opt_x, X_opt_x, U_opt_x] = mpc_x.get_u(x0_x);
+U_opt_x(:,end+1) = NaN; % Pad last input with NaN
+
+% no linearisation point here
+ph_x = rocket.plotvis_sub(T_opt_x, X_opt_x, U_opt_x, sysx, xs, us);
+
+%sysy
+[u_y, T_opt_y, X_opt_y, U_opt_y] = mpc_y.get_u(x0_y);
+U_opt_y(:,end+1) = NaN; % Pad last input with NaN
+
+% no linearisation point here
+ph_y = rocket.plotvis_sub(T_opt_y, X_opt_y, U_opt_y, sysy, xs, us);
+
+%sysroll
+[u_roll, T_opt_roll, X_opt_roll, U_opt_roll] = mpc_roll.get_u(x0_roll);
+U_opt_roll(:,end+1) = NaN; % Pad last input with NaN
+
+% no linearisation point here
+ph_roll = rocket.plotvis_sub(T_opt_roll, X_opt_roll, U_opt_roll, sysroll, xs, us);
+
+% z (this has a trim point)
+[u_z, T_opt_z, X_opt_z, U_opt_z] = mpc_z.get_u(x0_z);
+U_opt_z(:,end+1) = NaN; % Pad last input with NaN
+
+% no linearisation point here
+ph_z = rocket.plotvis_sub(T_opt_z, X_opt_z, U_opt_z, sysz, xs, us);
