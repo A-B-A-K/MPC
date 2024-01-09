8a608be667a1ea4ba26f6221dc453f79f2f4565e
marpep <127655372+marpep@users.noreply.github.com>
2024-01-09 17:10:23 +0100

    Add files via upload

--- rocket_project/Deliverable_5_2/Deliverable_5_2.m
+++ rocket_project/Deliverable_5_2/Deliverable_5_2.m
@@ -1,7 +1,45 @@
 addpath(fullfile('..', 'src'));
 
-%close all
-%clear all
-%clc
+close all
+clear all
+clc
 
 %% TODO: This file should produce all the plots for the deliverable
+
+% Define subsystems
+Ts = 1/20;
+
+% Choose simulation time
+Tf = 20;
+rocket= Rocket(Ts); 
+[xs,us] =rocket.trim(); %Compute steady−state for which 0=f(xs,us) 
+sys=rocket.linearize(xs,us); %Linearize the non linear model about trim point
+
+[sysx, sysy, sysz, sysroll]=rocket.decompose(sys,xs,us);
+
+H = 5;
+mpc_x = MpcControl_x(sysx, Ts, H);
+mpc_y = MpcControl_y(sysy, Ts, H);
+mpc_z = MpcControl_z(sysz, Ts, H);
+mpc_roll = MpcControl_roll(sysroll, Ts, H);
+
+% Merge four sub−system controllers into one full−system controller
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+x0 = [zeros(1, 9), 1 0 3]';
+ref = [1.2, 0, 3, 0]';
+
+% Manipulate mass for simulation
+rocket.mass = 2.13;
+rocket.mass_rate = -0.27;
+
+% Using state estimates of observer
+[T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sysz);
+rocket.anim_rate = 5;
+ph = rocket.plotvis(T, X, U, Ref);
+
+
+% Not using state estimates from observer
+rocket.mass = 2.13;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
+rocket.anim_rate = 5;
+ph = rocket.plotvis(T, X, U, Ref);
